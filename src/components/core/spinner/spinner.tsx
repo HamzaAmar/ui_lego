61ffcdb4690da00b38b4ebbac22aa6bcdfe834ee
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2022-08-28 19:53:14 +0100

    feat: add spinner core components

---
+++ src/components/core/spinner/spinner.tsx
@@ -0,0 +1,15 @@
+import { classnames } from '../../../utils/classnames';
+import { SpinnerProps } from './spinner.type';
+
+const spinner = ({
+  color = 'secondary',
+  size = 'sm',
+  ...rest
+}: SpinnerProps) => {
+  const className = classnames(`spinner spinner__${size} u_${color}`, {
+    [`u_${color}`]: Boolean(color),
+  });
+  return <div {...className} {...rest} />;
+};
+
+export default spinner;
---
+++ src/components/core/spinner/spinner.type.ts
@@ -0,0 +1,13 @@
+type Color =
+  | 'primary'
+  | 'secondary'
+  | 'danger'
+  | 'warning'
+  | 'success'
+  | 'info';
+type Size = 'sm' | 'md' | 'lg';
+
+export interface SpinnerProps {
+  color?: Color;
+  size?: Size;
+}
